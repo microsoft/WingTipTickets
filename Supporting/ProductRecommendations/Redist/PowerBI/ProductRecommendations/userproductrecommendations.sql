8285672e6303294570cb0a6953db84690687d580
meyergm <meyer@prowesscorp.com>
2015-12-22 19:29:44 -0800

    Uploading Supporting product rec redist

---
+++ Supporting/ProductRecommendations/Redist/PowerBI/ProductRecommendations/userproductrecommendations.sql
@@ -0,0 +1,6 @@
+Select UserId, UserName, ProductId, ProductName, RecommendedProduct, RankSel from (
+select U.UserId as UserId, U.Name as UserName, PC.ProductId as ProductId, PC.Name as ProductName, R.Product2 as RecommendedProduct, RANK() OVER (PARTITION BY U.Name, PC.Name ORDER BY U.Name, PC.Name, R.Product2 DESC) AS RankSel
+From ProductsCatalog PC, ProductsUsage PU, Users U, Recommendations R
+Where PC.productid = PU.productid
+AND PU.UserId = U.UserID
+AND PC.Name = R.Product1) A
