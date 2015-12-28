351d11c0696f49e480aeedececec27154e5799b6
meyergm <meyer@prowesscorp.com>
2015-12-28 09:19:22 -0800

    Deleted WingTipTicketsOld Folder

--- WingTipTicketsOld/sourcecode/WingTipTickets/TenantProvisioning.Core/Models/AzureComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace TenantProvisioning.Core.Models
-{
-    public class AzureComponent
-    {
-        public string Name { get; set; }
-        public string Service { get; set; }
-        public bool Exists { get; set; }
-    }
-}
