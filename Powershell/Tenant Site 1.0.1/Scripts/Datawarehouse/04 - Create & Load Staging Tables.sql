a90efbe4add54cc2c17a39e90a9eb9f68b691cc4
meyergm <meyer@prowesscorp.com>
2016-01-19 14:10:54 -0800

    Updated SQL Scripts

--- Powershell/Tenant Site 1.0.1/Scripts/Datawarehouse/04 - Create & Load Staging Tables.sql
+++ Powershell/Tenant Site 1.0.1/Scripts/Datawarehouse/04 - Create & Load Staging Tables.sql
@@ -23,49 +23,6 @@ FROM asb.DimCurrency;
 -- Creating Dim External Table
 -- ================================================================
 
-IF OBJECT_ID('stage.DimCustomer') IS NOT NULL
-    DROP TABLE stage.DimCustomer;
-GO
-
-CREATE TABLE stage.DimCustomer
-WITH (DISTRIBUTION = HASH(CustomerKey))
-AS
-SELECT
-    CustomerKey
-  , GeographyKey
-  , CustomerLabel
-  , Title
-  , FirstName
-  , MiddleName
-  , LastName
-  , NameStyle
-  , BirthDate
-  , MaritalStatus
-  , Suffix
-  , Gender
-  , EmailAddress
-  , YearlyIncome
-  , TotalChildren
-  , NumberChildrenAtHome
-  , Education
-  , Occupation
-  , HouseOwnerFlag
-  , NumberCarsOwned
-  , AddressLine1
-  , AddressLine2
-  , Phone
-  , DateFirstPurchase
-  , CustomerType
-  , CompanyName
-  , ETLLoadID
-  , LoadDate
-  , UpdateDate
-FROM asb.DimCustomer;
-
--- ================================================================
--- Creating Dim External Table
--- ================================================================
-
 IF OBJECT_ID('stage.DimDate') IS NOT NULL
     DROP TABLE stage.DimDate;
 GO
