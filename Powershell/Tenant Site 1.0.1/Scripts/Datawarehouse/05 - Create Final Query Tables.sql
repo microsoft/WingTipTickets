db93d9c9c8a1d9173ae17b42698caaf06e36da43
meyergm <meyer@prowesscorp.com>
2016-01-20 09:28:13 -0800

    Updating .sql scripts

--- Powershell/Tenant Site 1.0.1/Scripts/Datawarehouse/05 - Create Final Query Tables.sql
+++ Powershell/Tenant Site 1.0.1/Scripts/Datawarehouse/05 - Create Final Query Tables.sql
@@ -33,6 +33,61 @@ GO
 -- Creating Dim External Table
 -- ================================================================
 
+IF OBJECT_ID('dbo.DimCustomer') IS NOT NULL
+    DROP TABLE dbo.DimCustomer;
+GO
+
+
+CREATE TABLE dbo.DimCustomer
+WITH (DISTRIBUTION = HASH(CustomerKey), CLUSTERED COLUMNSTORE INDEX)
+AS
+SELECT
+    CustomerKey
+  , GeographyKey
+  , CustomerLabel
+  , Title
+  , FirstName
+  , MiddleName
+  , LastName
+  , NameStyle
+  , BirthDate
+  , MaritalStatus
+  , Suffix
+  , Gender
+  , EmailAddress
+  , YearlyIncome
+  , TotalChildren
+  , NumberChildrenAtHome
+  , Education
+  , Occupation
+  , HouseOwnerFlag
+  , NumberCarsOwned
+  , AddressLine1
+  , AddressLine2
+  , Phone
+  , DateFirstPurchase
+  , CustomerType
+  , CompanyName
+  , ETLLoadID
+  , LoadDate
+  , UpdateDate
+FROM stage.DimCustomer;
+GO
+
+ALTER TABLE dbo.DimCustomer REBUILD;
+GO
+
+UPDATE STATISTICS dbo.DimCustomer WITH FULLSCAN;
+GO
+
+CREATE STATISTICS S_CustomerKey ON dbo.DimCustomer (CustomerKey) WITH FULLSCAN;
+CREATE STATISTICS S_GeographyKey ON dbo.DimCustomer (GeographyKey) WITH FULLSCAN;
+CREATE STATISTICS S_CustomerType ON dbo.DimCustomer (CustomerType) WITH FULLSCAN;
+GO
+
+-- ================================================================
+-- Creating Dim External Table
+-- ================================================================
 IF OBJECT_ID('dbo.DimDate') IS NOT NULL
     DROP TABLE dbo.DimDate;
 GO
