b8d5a52a4a21d345523faa2dd1a250dfe49dbcd4
meyergm <meyer@prowesscorp.com>
2016-01-27 11:42:57 -0800

    Added Supporting\PowerBI\WTTPowerBI.pbix. Deleted Support\Datawarehouse\*sql scripts to update with correct scripts.
    Added Supporting\PowerBI\WTTPowerBI.pbix. Deleted
    Support\Datawarehouse\*sql scripts to update with correct scripts.

--- Supporting/DataWarehouse/Scripts/02 - Create Schemas.sql
+++
@@ -1,9 +0,0 @@
--- ================================================================
--- Creating external and staging schemas
--- ================================================================
-
-CREATE SCHEMA asb
-GO
-
-CREATE SCHEMA stage
-GO
