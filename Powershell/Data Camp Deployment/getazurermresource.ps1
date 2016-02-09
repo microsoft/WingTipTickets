e049f41af5179cba059a40b0d6b3b73b6f7c8d2b
meyergm <meyer@prowesscorp.com>
2016-02-09 09:06:24 -0800

    Committing meyergm fork changes to master

---
+++ Powershell/Data Camp Deployment/getazurermresource.ps1
@@ -0,0 +1,13 @@
+﻿$date = get-date -f yyyy-MM-dd
+Import-Csv "C:\WTTDailyReport\azuresubs.csv" | foreach ($_.subscriptionID) `
+{
+ Select-AzureRmSubscription -SubscriptionId $_.subscriptionID
+
+$ResourceGroup = Get-AzureRmResourceGroup
+If ($ResourceGroup -ne $null)
+{
+    Get-AzureRmResource -ExpandProperties | select Name, ResourceType, Sku | Export-Csv ("C:\WTTDailyReport\$_.SubscriptionID_($date).csv")
+
+}
+
+}
---
+++ Powershell/Tenant Site 1.0.1/New-WTTAzureTrafficManagerProfile.ps1
@@ -0,0 +1,73 @@
+<#
+.Synopsis
+	Azure Traffic Manager operation.
+.DESCRIPTION
+	This script is used to create an object in Azure Traffic Manager Profile.
+.EXAMPLE
+	New-WTTAzureTrafficManagerProfile -AzureTrafficManagerProfileName <string>
+#>
+function New-WTTAzureTrafficManagerProfile
+{
+	[CmdletBinding()]
+	Param
+	(    
+		# Azure Traffic Manager Profile Name
+		[Parameter(Mandatory=$true)]
+		[String]
+		$AzureTrafficManagerProfileName,
+
+		# Azure Resource Group Name
+		[Parameter(Mandatory=$true)]
+		[String]
+		$AzureTrafficManagerResourceGroupName
+	)
+
+	Process
+	{ 
+		# Build the Traffic Manager Profile Name
+		if ($AzureTrafficManagerProfileName.ToString().Contains(".trafficmanager.net"))
+		{
+			$AzureTrafficManagerDomainName = $AzureTrafficManagerProfileName
+			$AzureTrafficManagerProfileName = $AzureTrafficManagerProfileName.TrimEnd(".trafficmanager.net")   
+		}
+		else
+		{
+			$AzureTrafficManagerDomainName = $AzureTrafficManagerProfileName
+		}
+
+		Try 
+		{
+			# Check if Azure Traffic Manager Domain Name is Available
+			LineBreak
+			WriteLabel("Checking for Azure Traffic Manager '$AzureTrafficManagerDomainName'")
+			$azureTrafficManager = Find-AzureRmResource -ResourceType "Microsoft.Network/trafficmanagerprofiles" -ResourceNameContains $AzureTrafficManagerProfileName -ResourceGroupNameContains $AzureTrafficManagerResourceGroupName
+
+			if($azureTrafficManager -eq $null)
+			{
+				WriteValue("Not Found")
+
+				# Create Traffic Manage Profile
+				WriteLabel("Creating Traffic Manager Profile '$AzureTrafficManagerDomainName'")
+				#$newAzureTrafficManagerDomain = New-AzureTrafficManagerProfile -Name $azureTrafficManagerProfileName -DomainName $AzureTrafficManagerDomainName -LoadBalancingMethod Failover -MonitorPort 80 -MonitorProtocol Http -MonitorRelativePath "/" -Ttl 30
+				$newAzureTrafficManagerDomain = New-AzureRMTrafficManagerProfile -Name $AzureTrafficManagerProfileName -ResourceGroupName $AzureTrafficManagerResourceGroupName -RelativeDnsName $AzureTrafficManagerDomainName -TrafficRoutingMethod Weighted -MonitorPort 80 -MonitorProtocol HTTP -MonitorPath "/" -Ttl 30
+
+				if($newAzureTrafficManagerDomain.ProfileStatus -eq "Enabled")
+				{
+					WriteValue("Successful")
+				}
+				elseif($newAzureTrafficManagerDomain.ProfileStatus -eq "Failed")
+				{
+					WriteValue("Failed")
+				}
+			}
+			else
+			{
+				WriteValue("Found")
+			}
+		}
+		Catch
+		{
+			WriteError($Error)
+		}  	    
+	}
+}
