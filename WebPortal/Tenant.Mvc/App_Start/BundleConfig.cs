45cc2fe55727ba0a5dd3ebc197248f59eeab766f
wernerd@nvisionit.co.za <wernerd@nvisionit.co.za>
2016-03-29 13:35:09 +0300

    Fixed issue with javascript file references

--- WebPortal/Tenant.Mvc/App_Start/BundleConfig.cs
+++ WebPortal/Tenant.Mvc/App_Start/BundleConfig.cs
@@ -21,24 +21,34 @@ namespace Tenant.Mvc
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Content/Scripts/jquery-{version}.js",
                 "~/Content/Scripts/jquery.ui.widget.js",
+                "~/Content/Scripts/jquery.ui-1.11.4.js",
                 "~/Content/Scripts/jquery.fileupload.js",
                 "~/Content/Scripts/jquery.fileupload-ui.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Content/Scripts/bootstrap.js",
+                "~/Content/Scripts/bootstrap-select.js",
                 "~/Content/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/powerbi").Include(
+                "~/Content/Scripts/powerbi.js"));
         }
 
         private static void RegisterStyles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/styles/site").Include(
-                "~/Content/Stylesheets/site.css",
-                "~/Content/Stylesheets/recommendation.css",
-                "~/Content/Stylesheets/jquery.fileupload.css"));
-
             bundles.Add(new StyleBundle("~/styles/bootstrap").Include(
                 "~/Content/Stylesheets/bootstrap-theme.css",
                 "~/Content/Stylesheets/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/styles/jquery").Include(
+                "~/Content/Stylesheets/jquery.fileupload.css"));
+
+            bundles.Add(new StyleBundle("~/styles/powerbi").Include(
+                "~/Content/Stylesheets/powerbi.css"));
+
+            bundles.Add(new StyleBundle("~/styles/site").Include(
+                "~/Content/Stylesheets/site.css",
+                "~/Content/Stylesheets/recommendation.css"));
         }
 
         #endregion
