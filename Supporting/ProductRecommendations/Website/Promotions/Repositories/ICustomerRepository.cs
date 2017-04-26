3a116482c510d15398d4dd52388649feb53bb756
meyergm <meyer@prowesscorp.com>
2017-04-26 13:46:55 -0700

    Removed unnecessary files, uploaded latest HOL, Web App files, and Power BI Report

--- Supporting/ProductRecommendations/Website/Promotions/Repositories/ICustomerRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Promotions.Repositories
-{
-    public interface ICustomerRepository
-    {
-        Customer GetCustomerByName(string customer);
-        Customer GetCustomerById(Int64 id);
-    }
-}
--- Supporting/ProductRecommendations/Website/Promotions/Repositories/ITelemetryRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Promotions.Repositories
-{
-    public interface ITelemetryRepository
-    {
-        void SendClick(Promotions.Events.ClickEvent click);
-        void SendPurchase(Promotions.Events.PurchaseEvent purchase);
-    }
-}
